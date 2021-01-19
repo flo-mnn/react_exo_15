842d7a319d601eac37958a5c4c415e5296d2b376
Florence <monnaie.florence@gmail.com>
2021-01-19 10:40:29 +0100

    exo 16 done

--- src/Article.js
+++ src/Article.js
@@ -1,16 +1,19 @@
 import React, {useState} from 'react';
 
 let Article = ({book}) => {
+
+    const [oeuvre, setOeuvre] = useState(book);
+
     return(
         <div id="allArticles">
     
             {
-                book[0].map((titre,index)=>{
+                oeuvre[0].map((titre,index)=>{
                     return (
                         <div id="article-item">
                             <span>{index +1}</span>
                             <h1 key={index}>{titre.title}</h1>
-                            <span>{book[1][index].author}</span> <br/> <br/>
+                            <span>{oeuvre[1][index].author}</span> <br/> <br/>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deleniti quos assumenda sit commodi amet ut tenetur rerum laudantium non. Impedit, pariatur accusamus quod nulla unde voluptates magnam aspernatur fuga!</p>
                         </div>
                     )
